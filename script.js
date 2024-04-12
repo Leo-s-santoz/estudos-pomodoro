7279e6f93c77332460ff8a0e40b01a54e044ba42
Leonardo Santos <leonardo@Leonardos-MacBook-Air.local>
2024-04-12 17:45:16 -0300

    adicionado mudança de aparencia de acordo com seleção

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const iniciarBt = document.querySelector(".app__card-primary-button");
 const focoBt = document.querySelector(`.app__card-button--foco`);
 const curtoBt = document.querySelector(`.app__card-button--curto`);
 const longoBt = document.querySelector(`.app__card-button--longo`);
+const botoes = document.querySelectorAll(`.app__card-button`);
 //elementos da página
 const timer = document.querySelector(`#timer`);
 const imagem = document.querySelector(`.app__image`);
@@ -15,17 +16,23 @@ const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener(`click`, () => {
   alterarContexto(`foco`);
+  focoBt.classList.add(`active`);
 });
 
 curtoBt.addEventListener(`click`, () => {
   alterarContexto(`descanso-curto`);
+  curtoBt.classList.add(`active`);
 });
 
 longoBt.addEventListener(`click`, () => {
   alterarContexto(`descanso-longo`);
+  longoBt.classList.add(`active`);
 });
 
 function alterarContexto(contexto) {
+  botoes.forEach((e) => {
+    e.classList.remove(`active`);
+  });
   html.setAttribute(`data-contexto`, contexto);
   imagem.setAttribute(`src`, `/imagens/${contexto}.png`);
   switch (contexto) {
